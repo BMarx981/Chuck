fd1ab9c221152222fc9de23b55cb72e5e696052e
Brian Marx <b.marx981@gmail.com>
2018-02-14 21:34:04 -0500

    Initial Commit

---
+++ Chuck/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Chuck
+//
+//  Created by Marx, Brian on 2/14/18.
+//  Copyright © 2018 Marx, Brian. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Chuck/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chuck
+//
+//  Created by Marx, Brian on 2/14/18.
+//  Copyright © 2018 Marx, Brian. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
